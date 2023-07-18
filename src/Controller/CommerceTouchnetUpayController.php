a61e5ba88c3ede379b4ec9284e81e8df6650a3c2
Joël Pittet <pittet@cs.ubc.ca>
2023-07-18 10:15:03 -0700

    Add success, error, cancel pages

--- src/Controller/CommerceTouchnetUpayController.php
+++ src/Controller/CommerceTouchnetUpayController.php
@@ -12,11 +12,37 @@ class CommerceTouchnetUpayController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function build() {
+  public function successPage() {
     // @todo Add real controller responses and routing or delete.
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t('Your payment was successful.'),
+    ];
+
+    return $build;
+  }
+
+  /**
+   * Builds the payment error response.
+   */
+  public function errorPage() {
+    // @todo Add real controller responses and routing or delete.
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('There was an error with your payment'),
+    ];
+
+    return $build;
+  }
+
+  /**
+   * Builds the cancelled payment response.
+   */
+  public function cancelPage() {
+    // @todo Add real controller responses and routing or delete.
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('You cancelled your payment'),
     ];
 
     return $build;
