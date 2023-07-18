581ca251222e484485f9add39b23754d71959616
Joël Pittet <pittet@cs.ubc.ca>
2023-07-18 10:15:44 -0700

    move logic to the onNotify method from onReturn for IPN

--- src/Plugin/Commerce/PaymentGateway/OffsiteRedirect.php
+++ src/Plugin/Commerce/PaymentGateway/OffsiteRedirect.php
@@ -5,9 +5,11 @@ namespace Drupal\commerce_touchnet_upay\Plugin\Commerce\PaymentGateway;
 use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayInterface;
+use Drupal\commerce_price\Price;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Provides the off-site payment gateway for UBC TouchNet uPay proxy.
@@ -79,6 +81,36 @@ class OffsiteRedirect extends OffsitePaymentGatewayBase implements OffsitePaymen
    * {@inheritdoc}
    */
   public function onReturn(OrderInterface $order, Request $request) {
+    // Intentionally left blank, all IPN will be handled via self::onNotify().
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onCancel(OrderInterface $order, Request $request) {
+    // @todo Replace if need a better message, or remove.
+    parent::onCancel($order, $request);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onNotify(Request $request):Response|null {
+    // @todo Only create payment entities on successful payments.
+    // 1. Request is validated
+    // If validation fails: return new JsonResponse($exception->getMessage(), $exception->getCode());
+
+
+    // @todo This method is responsible for:
+    //
+    //    performing verifications, throwing exceptions as needed
+    //    creating and saving information to the Drupal Commerce payment for the order
+    //
+    // Typically, you will also want to log the information returned by the provider.
+    //    This method should only be concerned with creating/completing payments.
+    // @see https://docs.drupalcommerce.org/commerce2/developer-guide/payments/create-payment-gateway/off-site-gateways/return-from-payment-provider#handling-payment-submission
+
+    $metadata = $resource->metadata;
     // @todo Add examples of request validation.
     // Note: Since requires_billing_information is FALSE, the order is
     // not guaranteed to have a billing profile. Confirm that
@@ -86,29 +118,24 @@ class OffsiteRedirect extends OffsitePaymentGatewayBase implements OffsitePaymen
     $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
     $payment = $payment_storage->create([
       'state' => 'completed',
-      'amount' => $order->getBalance(),
+      'amount' => new Price($resource->amount / 100, $resource->currency),
+//      'amount' => $order->getBalance(),
+      // 'amount' => $order->getTotalPrice(),
       'payment_gateway' => $this->parentEntity->id(),
-      'order_id' => $order->id(),
+//      'order_id' => $order->id(),
+      'order_id' => $metadata['order_id'],
+      'test' => $this->getMode() == 'test',
       'remote_id' => $request->query->get('txn_id'),
       'remote_state' => $request->query->get('payment_status'),
+//      'remote_id' => $resource->id,
+//      'remote_state' => empty($resource->failure) ? 'paid' : $resource->failure->code,
     ]);
+    // $logger->info('Saving Payment information. Transaction reference: ' . $merchantTransactionReference);
     $payment->save();
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function onCancel(OrderInterface $order, Request $request) {
-    $this->messenger()->addMessage($this->t('You have canceled checkout at @gateway but may resume the checkout process here when you are ready.', [
-      '@gateway' => $this->getDisplayLabel(),
-    ]));
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function onNotify(Request $request) {
-
+    // drupal_set_message('Payment was processed');
+    //
+    //    $logger->info('Payment information saved successfully. Transaction reference: ' . $merchantTransactionReference);.
   }
 
 }
